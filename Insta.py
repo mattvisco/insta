50d393618bd2d7a3c79fd2256d7c106ca628aa10
sitraka- <sitraka.hr@gmail.com>
2016-03-15 00:59:40 +0000

    added css to style, modified index.html, added pyCurl for instagramrequest + response parsing in insta.py

--- Insta.py
+++ Insta.py
@@ -7,6 +7,91 @@ from contextlib import closing
 from werkzeug import secure_filename
 # from juggernaut import Juggernaut
 
+#pycURL imports
+import pycurl, json
+import re
+import StringIO
+import string
+
+# Login and Crawling for #bestview(->to be changed for #emojimilan in final)  
+b = StringIO.StringIO()
+c = pycurl.Curl()
+c.setopt(pycurl.URL, 'https://instagram.com/accounts/login/?force_classic_login')
+c.setopt(pycurl.REFERER, 'https://instagram.com/accounts/login/?force_classic_login')
+c.setopt(pycurl.FOLLOWLOCATION, True)
+c.setopt(pycurl.SSL_VERIFYPEER, False)
+c.setopt(pycurl.WRITEFUNCTION, b.write)
+c.setopt(pycurl.COOKIEFILE, 'cookiess.txt')
+c.setopt(pycurl.COOKIEJAR, 'cookiess.txt')
+page = c.perform()
+c.close()
+
+links = re.findall('<input type="hidden" name="csrfmiddlewaretoken" value="([A-z0-9]{32})"/>', str(page))
+
+c = pycurl.Curl()
+c.setopt(pycurl.URL, 'https://instagram.com/accounts/login/?force_classic_login')
+c.setopt(pycurl.REFERER, 'https://instagram.com/accounts/login/?force_classic_login')
+c.setopt(pycurl.WRITEFUNCTION, b.write)
+c.setopt(pycurl.POST, True)
+c.setopt(pycurl.SSL_VERIFYPEER, False)
+c.setopt(pycurl.FOLLOWLOCATION, True)
+c.setopt(pycurl.POSTFIELDS, "csrfmiddlewaretoken=336ce9019884103a0a3a87dd639c1803&username=[sitrakahr]&password=[Qol6lecx]")
+c.setopt(pycurl.COOKIEFILE, 'cookiess.txt')
+c.setopt(pycurl.COOKIEJAR, 'cookiess.txt')
+page = c.perform()
+c.close()
+
+c = pycurl.Curl()
+c.setopt(pycurl.URL, 'https://www.instagram.com/explore/tags/bestview/')
+c.setopt(pycurl.REFERER, 'https://instagram.com/')
+c.setopt(pycurl.HTTPHEADER, [
+        'Accept-Language: en-US,en;q=0.8',
+        'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.132 Safari/537.36',
+        'Accept: */*',
+        'X-Requested-With: XMLHttpRequest',
+        'Connection: keep-alive'
+        ])
+c.setopt(pycurl.WRITEFUNCTION, b.write)
+c.setopt(pycurl.SSL_VERIFYPEER, False)
+c.setopt(pycurl.FOLLOWLOCATION, True)
+c.setopt(pycurl.COOKIEFILE, 'cookiess.txt')
+c.setopt(pycurl.COOKIEJAR, 'cookiess.txt')
+page = c.perform()
+c.close()
+
+# retrieve the values printed by b
+rawpage = b.getvalue()
+firstsplit = rawpage.split('window._sharedData')
+displaysrc = firstsplit[3].split('"display_src":"')
+displayID = firstsplit[3].split(',"id":"')
+
+displaysrc_length = len(displaysrc)-1
+
+# store values in lists
+raw_src = []
+list_ID = []
+
+for i in range(1,displaysrc_length):
+    rawsrc = displaysrc[i].split('?ig_cache_key')
+    rawID = displayID[i].split('","display_src"')
+    raw_src.append(rawsrc[0])
+    list_ID.append(rawID[0])
+    #print rawID[0]
+    
+raw_src_length = len(raw_src)-1
+list_src = []
+the_list = []
+
+for src in range(0, raw_src_length):
+    srcreplaced = raw_src[src].replace("\/", "/")
+    list_src.append(srcreplaced)
+    tmp_list = [srcreplaced, list_ID[src]]
+    the_list.append(tmp_list)
+    #print srcreplaced
+
+#for l in range(0,len(the_list)-1):
+ #   print the_list[l]
+
 # configuration
 DATABASE = '/tmp/insta.db'
 DEBUG = True
@@ -19,12 +104,14 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('INSTA_SETTINGS', silent=True)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_FOLDER = os.path.join(APP_ROOT, 'static/')
 LIKES_FOLDER = os.path.join(APP_ROOT, 'static/like/')
 SUPER_FOLDER = os.path.join(APP_ROOT, 'static/super/')
 EDITED_SUPER_FOLDER = os.path.join(APP_ROOT, 'static/edited_super/')
 app.config['like'] = LIKES_FOLDER
 app.config['super'] = SUPER_FOLDER
 app.config['edited_super'] = EDITED_SUPER_FOLDER
+app.config['static'] = STATIC_FOLDER
 SCREEN_TOTAL = 7
 
 # jug = Juggernaut()
@@ -57,7 +144,7 @@ def query_db(query, args=(), one=False):
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    return render_template('index.html', src=the_list)
 
 @app.route('/store/<img_type>/<insta_id>', methods=['POST'])
 def store(img_type, insta_id=None):
