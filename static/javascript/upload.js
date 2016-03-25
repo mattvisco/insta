3d9a20ddc1385ee3ab499a034ac14e58019ba383
Matt Visco <mattvisco@mac.com>
2016-03-25 14:11:09 -0700

    updated UI for upload

--- static/javascript/upload.js
+++ static/javascript/upload.js
@@ -3,16 +3,38 @@
  */
 
 var socket;
+var loading = false;
+
 $(function() {
     socket = io.connect('http://' + document.domain + ':' + location.port + '/slides');
     socket.on('connect', function() {
         console.log('joined')
     });
 
+    $('input[type=submit]').click(function(){
+        if($('file-to-send').files.length == 0 || loading) {
+            return false;
+        } else {
+            $('#check').hide();
+            $('#fail').hide();
+            $('#spin').show();
+            loading = true;
+            $(this).css({backgroundColor: 'grey', cursor: 'not-allowed'});
+        }
+    });
+
     $('#super-upload').ajaxForm(function(data) {
-        if(data.error) alert(data.error);
-        else {
-            alert('Success');
+        loading = false;
+        $('input[type=submit]').css({backgroundColor: '', cursor: ''});
+        if(data.error) {
+            $('#spin').hide();
+            $('#check').hide();
+            $('#fail').show();
+            alert(data.error);
+        } else {
+            $('#spin').hide();
+            $('#fail').hide();
+            $('#check').show();
             socket.emit('upload complete', data);
         }
     });
